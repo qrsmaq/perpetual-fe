866024d06aa48bb631be8d6ffcf36828e1fe3c45
SFMack <sfcamack@icloud.com>
2021-05-28 10:52:05 -0700

    base structure implmented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { Suspense, lazy } from "react"; // loads content as needed
+import { Route, Switch } from "react-router-dom";
+
+const Auth = lazy(() => import("./components/Auth/Auth"));
+const Dashboard = lazy(() => import("./components/Dashboard/Dashboard"));
+const Home = lazy(() => import("./components/Home/Home"));
+
 function App() {
   return (
     <div className="App">
-      <h1>App</h1>
+      <Suspense fallback={<div>Page is Loading...</div>}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/auth" component={Auth} />
+          <Route path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Suspense>
     </div>
   );
 }
---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,19 @@
+import React from "react";
+// import Login from "./Login/Login";
+// import Register from "./Register/Register";
+
+export const Auth = () => {
+  return (
+    <div className="bg-gradient-primary vw-100 vh-100">
+      <div className="container">
+        <div className="card o-hidden border-0 shadow-lg my-5">
+          <div className="card-body p-0">
+            {/* render form based on redux state */}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
